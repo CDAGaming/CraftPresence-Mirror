d5e51572ed9f16875375a14f447985d4ad157dd5
CDAGaming <cstack2011@yahoo.com>
2022-12-09 14:16:29 -0600

    [Fix] Fixed multiple function issues
    - Apparently the stack is popped backwards, not forwards

--- src/main/java/com/gitlab/cdagaming/craftpresence/utils/discord/FunctionsLib.java
+++ src/main/java/com/gitlab/cdagaming/craftpresence/utils/discord/FunctionsLib.java
@@ -69,18 +69,6 @@ public class FunctionsLib {
     public static Value rgbaToHex(Starscript ss, int argCount) {
         if (argCount < 3 || argCount > 4)
             ss.error("rgbaToHex() can only be used with 3-4 arguments, got %d.", argCount);
-        int r = (int) ss.popNumber("First argument to rgbaToHex() needs to be a number.");
-        if (r < 0 || r > 255) {
-            ss.error("First argument to rgbaToHex() is not a valid color index, can only be 0-255.");
-        }
-        int g = (int) ss.popNumber("Second argument to rgbaToHex() needs to be a number.");
-        if (g < 0 || g > 255) {
-            ss.error("Second argument to rgbaToHex() is not a valid color index, can only be 0-255.");
-        }
-        int b = (int) ss.popNumber("Third argument to rgbaToHex() needs to be a number.");
-        if (b < 0 || b > 255) {
-            ss.error("Third argument to rgbaToHex() is not a valid color index, can only be 0-255.");
-        }
         int a = 255;
         if (argCount == 4) {
             a = (int) ss.popNumber("Fourth argument to rgbaToHex() needs to be a number.");
@@ -88,25 +76,37 @@ public class FunctionsLib {
                 ss.error("Fourth argument to rgbaToHex() is not a valid color index, can only be 0-255.");
             }
         }
+        int b = (int) ss.popNumber("Third argument to rgbaToHex() needs to be a number.");
+        if (b < 0 || b > 255) {
+            ss.error("Third argument to rgbaToHex() is not a valid color index, can only be 0-255.");
+        }
+        int g = (int) ss.popNumber("Second argument to rgbaToHex() needs to be a number.");
+        if (g < 0 || g > 255) {
+            ss.error("Second argument to rgbaToHex() is not a valid color index, can only be 0-255.");
+        }
+        int r = (int) ss.popNumber("First argument to rgbaToHex() needs to be a number.");
+        if (r < 0 || r > 255) {
+            ss.error("First argument to rgbaToHex() is not a valid color index, can only be 0-255.");
+        }
         return Value.string(StringUtils.getHexFromColor(new Color(r, g, b, a)));
     }
 
     public static Value getOrDefault(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 2)
             ss.error("getOrDefault() can only be used with 1-2 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to getOrDefault() needs to be a string.");
         String alternative = "";
         if (argCount == 2) {
             alternative = ss.popString("Second argument to getOrDefault() needs to be a string.");
         }
+        String target = ss.popString("First argument to getOrDefault() needs to be a string.");
         return Value.string(StringUtils.getOrDefault(target, alternative));
     }
 
     public static Value replaceAnyCase(Starscript ss, int argCount) {
         if (argCount != 3) ss.error("replaceAnyCase() requires 3 arguments, got %d.", argCount);
-        String source = ss.popString("First argument to replaceAnyCase() needs to be a string.");
-        String from = ss.popString("Second argument to replaceAnyCase() needs to be a string.");
         String to = ss.popString("Third argument to replaceAnyCase() needs to be a string.");
+        String from = ss.popString("Second argument to replaceAnyCase() needs to be a string.");
+        String source = ss.popString("First argument to replaceAnyCase() needs to be a string.");
         return Value.string(StringUtils.replaceAnyCase(source, from, to));
     }
 
@@ -118,8 +118,8 @@ public class FunctionsLib {
 
     public static Value minify(Starscript ss, int argCount) {
         if (argCount != 2) ss.error("minify() requires 2 arguments, got %d.", argCount);
-        String source = ss.popString("First argument to minify() needs to be a string.");
         int length = (int) ss.popNumber("Second argument to minify() needs to be a number.");
+        String source = ss.popString("First argument to minify() needs to be a string.");
         return Value.string(StringUtils.minifyString(source, length));
     }
 
@@ -133,22 +133,22 @@ public class FunctionsLib {
     public static Value nullOrEmpty(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 2)
             ss.error("nullOrEmpty() can only be used with 1-2 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to nullOrEmpty() needs to be a string.");
         boolean allowWhitespace = false;
         if (argCount == 2) {
             allowWhitespace = ss.popBool("Second argument to nullOrEmpty() needs to be a boolean.");
         }
+        String target = ss.popString("First argument to nullOrEmpty() needs to be a string.");
         return Value.bool(StringUtils.isNullOrEmpty(target, allowWhitespace));
     }
 
     public static Value formatAddress(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 2)
             ss.error("formatAddress() can only be used with 1-2 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to formatAddress() needs to be a string.");
         boolean returnPort = false;
         if (argCount == 2) {
             returnPort = ss.popBool("Second argument to formatAddress() needs to be a boolean.");
         }
+        String target = ss.popString("First argument to formatAddress() needs to be a string.");
         return Value.string(StringUtils.formatAddress(target, returnPort));
     }
 
@@ -191,19 +191,19 @@ public class FunctionsLib {
     public static Value asProperWord(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 4)
             ss.error("asProperWord() can only be used with 1-4 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to asProperWord() needs to be a string.");
-        boolean avoid = false;
-        if (argCount >= 2) {
-            avoid = ss.popBool("Second argument to asProperWord() needs to be a boolean.");
+        int caseCheckTimes = -1;
+        if (argCount == 4) {
+            caseCheckTimes = (int) ss.popNumber("Fourth argument to asProperWord() needs to be a number.");
         }
         boolean skipSymbolReplacement = false;
         if (argCount >= 3) {
             skipSymbolReplacement = ss.popBool("Third argument to asProperWord() needs to be a boolean.");
         }
-        int caseCheckTimes = -1;
-        if (argCount == 4) {
-            caseCheckTimes = (int) ss.popNumber("Fourth argument to asProperWord() needs to be a number.");
+        boolean avoid = false;
+        if (argCount >= 2) {
+            avoid = ss.popBool("Second argument to asProperWord() needs to be a boolean.");
         }
+        String target = ss.popString("First argument to asProperWord() needs to be a string.");
         return Value.string(StringUtils.formatWord(target, avoid, skipSymbolReplacement, caseCheckTimes));
     }
 
@@ -216,34 +216,34 @@ public class FunctionsLib {
     public static Value asIdentifier(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 3)
             ss.error("asIdentifier() can only be used with 1-3 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to asIdentifier() needs to be a string.");
-        boolean formatToId = false;
-        if (argCount >= 2) {
-            formatToId = ss.popBool("Second argument to asIdentifier() needs to be a boolean.");
-        }
         boolean avoid = false;
         if (argCount == 3) {
             avoid = ss.popBool("Third argument to asIdentifier() needs to be a boolean.");
         }
+        boolean formatToId = false;
+        if (argCount >= 2) {
+            formatToId = ss.popBool("Second argument to asIdentifier() needs to be a boolean.");
+        }
+        String target = ss.popString("First argument to asIdentifier() needs to be a string.");
         return Value.string(StringUtils.formatIdentifier(target, formatToId, avoid));
     }
 
     public static Value convertTime(Starscript ss, int argCount) {
         if (argCount != 3) ss.error("convertTime() requires 3 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to convertTime() needs to be a string.");
-        String originalPattern = ss.popString("Second argument to convertTime() needs to be a string.");
         String newPattern = ss.popString("Third argument to convertTime() needs to be a string.");
+        String originalPattern = ss.popString("Second argument to convertTime() needs to be a string.");
+        String target = ss.popString("First argument to convertTime() needs to be a string.");
         return Value.string(StringUtils.convertTime(target, originalPattern, newPattern));
     }
 
     public static Value capitalizeWords(Starscript ss, int argCount) {
         if (argCount < 1 || argCount > 2)
             ss.error("capitalizeWords() can only be used with 1-2 arguments, got %d.", argCount);
-        String target = ss.popString("First argument to capitalizeWords() needs to be a string.");
         int timesToCheck = -1;
         if (argCount == 2) {
             timesToCheck = (int) ss.popNumber("Second argument to capitalizeWords() needs to be a number.");
         }
+        String target = ss.popString("First argument to capitalizeWords() needs to be a string.");
         return Value.string(StringUtils.capitalizeWord(target, timesToCheck));
     }
 
@@ -256,9 +256,9 @@ public class FunctionsLib {
             instance = ss.popObject("First argument to getField() needs to be an object.");
             result = StringUtils.lookupObject(instance.getClass(), instance, fieldName);
         } else {
-            className = ss.popString("First argument to getField() needs to be a string.");
             fieldName = ss.popString("Third argument to getField() needs to be a string.");
             instance = ss.popObject("Second argument to getField() needs to be an object.");
+            className = ss.popString("First argument to getField() needs to be a string.");
             result = StringUtils.lookupObject(className, instance, fieldName);
         }
 
@@ -288,8 +288,8 @@ public class FunctionsLib {
      */
     public static Value hasField(Starscript ss, int argCount) {
         if (argCount != 2) ss.error("hasField() requires 2 arguments, got %d.", argCount);
-        String a = ss.popString("First argument to hasField() needs to be a string.");
         String b = ss.popString("Second argument to hasField() needs to be a string.");
+        String a = ss.popString("First argument to hasField() needs to be a string.");
         return Value.bool(StringUtils.doesClassContainField(a, b));
     }
 
