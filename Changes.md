676a1844f3db3aef97e1f640a8b9473544897c4a
CDAGaming <cstack2011@yahoo.com>
2024-06-01 12:15:16 -0500

    [Change] Bumped JVMDowngrader to `0.6.1`

--- Changes.md
+++ Changes.md
@@ -11,7 +11,7 @@ See the Mod Description or [README](https://gitlab.com/CDAGaming/CraftPresence) 
 
 * (Backend) Updated Build Dependencies (Please see the appropriate repositories for changes)
     * Unimined (`1.2.3` -> `1.2.7-SNAPSHOT`)
-    * JVMDowngrader (`local` -> `0.5.1`)
+    * JVMDowngrader (`local` -> `0.6.1`)
     * ModPublisher (`2.1.1` -> `2.1.2`)
     * Fabric Loader (`0.15.10` -> `0.15.11`)
     * Lenni Reflect (`1.3.2` -> `1.3.4`)
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     java
     id("xyz.wagyourtail.unimined") version "1.2.7-SNAPSHOT" apply false
-    id("xyz.wagyourtail.jvmdowngrader") version "0.5.1"
+    id("xyz.wagyourtail.jvmdowngrader") version "0.6.1"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("com.hypherionmc.modutils.modfusioner") version "1.0.10"
