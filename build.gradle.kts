dbbaf58993ba0ba31642db46264bf54131928b40
CDAGaming <cstack2011@yahoo.com>
2024-06-17 00:01:32 -0500

    [Change] Updated JVMDowngrader to `0.7.2`
    - Fixes runtime crashes for certain ASM versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     java
     id("xyz.wagyourtail.unimined") version "1.2.9" apply false
-    id("xyz.wagyourtail.jvmdowngrader") version "0.7.1"
+    id("xyz.wagyourtail.jvmdowngrader") version "0.7.2"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
     id("io.github.goooler.shadow") version "8.1.7" apply false
     id("com.hypherionmc.modutils.modfusioner") version "1.0.12"
