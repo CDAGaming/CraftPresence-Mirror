afa11a736ab94cc06652ce02af2c2d7e9b1a3268
CDAGaming <cstack2011@yahoo.com>
2024-07-09 22:35:17 -0500

    [Port] Update to special/rift (v2.5.3)

---
+++ rift/src/main/java/com/gitlab/cdagaming/craftpresence/rift/CraftPresenceRift.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 - 2025 CDAGaming (cstack2011@yahoo.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.gitlab.cdagaming.craftpresence.rift;
+
+import com.gitlab.cdagaming.craftpresence.CraftPresence;
+import com.gitlab.cdagaming.craftpresence.core.Constants;
+import io.github.cdagaming.unicore.utils.MappingUtils;
+import io.github.cdagaming.unicore.utils.OSUtils;
+import org.dimdev.rift.listener.MinecraftStartListener;
+import org.dimdev.riftloader.RiftLoader;
+
+/**
+ * The Primary Application Class and Utilities
+ *
+ * @author CDAGaming
+ */
+public class CraftPresenceRift implements MinecraftStartListener {
+    @Override
+    public void onMinecraftStart() {
+        try {
+            if (OSUtils.JAVA_SPEC < 1.8f) {
+                throw new UnsupportedOperationException("Incompatible JVM!!! @MOD_NAME@ requires Java 8 or above to work properly!");
+            }
+            new CraftPresence(this::setupIntegrations);
+        } catch (NoClassDefFoundError ex) {
+            throw new UnsupportedOperationException("Unable to initialize @MOD_NAME@! @UNILIB_NAME@ (unilib) is required to run this mod (Requires @UNILIB_MIN_VERSION@ or above)", ex);
+        }
+    }
+
+    /**
+     * Setup external integrations and attachments to the primary application
+     */
+    public void setupIntegrations() {
+        // N/A
+    }
+}
