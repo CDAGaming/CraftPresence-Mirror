c49d802ae9b0a4c00fc82eb015c4b3e63480716f
CDAGaming <cstack2011@yahoo.com>
2024-07-11 15:09:42 -0500

    [Port] Update to special/bta-7.1 (v2.5.0)

--- common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/server/ServerUtils.java
+++ common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/server/ServerUtils.java
@@ -478,7 +478,7 @@ public class ServerUtils implements ExtendedModule {
         try {
             if (CraftPresence.player != null) {
                 CraftPresence.world.sendQuittingDisconnectingPacket();
-                CraftPresence.instance.changeWorld1(null);
+                CraftPresence.instance.changeWorld(null);
             }
 
             RenderUtils.openScreen(
@@ -502,8 +502,8 @@ public class ServerUtils implements ExtendedModule {
         syncArgument("player.position.z", () -> MathUtils.roundDouble(CraftPresence.player.z, 3));
 
         // Player Health Arguments
-        syncArgument("player.health.current", () -> MathUtils.roundDouble(CraftPresence.player.health, 0));
-        syncArgument("player.health.max", () -> MathUtils.roundDouble(20.0D, 0));
+        syncArgument("player.health.current", () -> MathUtils.roundDouble(CraftPresence.player.getHealth(), 0));
+        syncArgument("player.health.max", () -> MathUtils.roundDouble(CraftPresence.player.getMaxHealth(), 0));
 
         // World Data Arguments
         syncArgument("world.difficulty", () -> {
