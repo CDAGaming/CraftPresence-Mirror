281599416c909d9e803c9fd7fc4fd03ce50e367e
CDAGaming <cstack2011@yahoo.com>
2024-07-21 17:35:22 -0500

    [Change] Updated Dependencies
    - Also cleaned up some obsolete properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ import java.util.*
 plugins {
     java
     id("xyz.wagyourtail.unimined") version "1.3.3" apply false
-    id("xyz.wagyourtail.jvmdowngrader") version "0.8.3"
+    id("xyz.wagyourtail.jvmdowngrader") version "0.9.0"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
-    id("io.github.goooler.shadow") version "8.1.7" apply false
+    id("io.github.goooler.shadow") version "8.1.8" apply false
     id("com.hypherionmc.modutils.modpublisher") version "2.1.6" apply false
 }
 
