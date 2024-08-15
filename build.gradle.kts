a1234215ee7737efd46dd004a4c3ea42497608e9
CDAGaming <cstack2011@yahoo.com>
2024-08-15 18:45:55 -0500

    [Change] Updated Dependencies
    - Updated UniLib build version to 1.0.2
    - Updated Unimined to 1.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import java.util.*
 
 plugins {
     java
-    id("xyz.wagyourtail.unimined") version "1.3.5" apply false
+    id("xyz.wagyourtail.unimined") version "1.3.6" apply false
     id("xyz.wagyourtail.jvmdowngrader") version "1.1.0"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
     id("com.gradleup.shadow") version "8.3.0" apply false
