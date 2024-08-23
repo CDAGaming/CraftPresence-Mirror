52791c50e94722e825917602eb3fb64928742a1f
CDAGaming <cstack2011@yahoo.com>
2024-08-23 11:31:08 -0500

    [Change] Updated Dependencies
    - Updated Unimined to 1.3.9
    - Updated ClassGraph to 4.8.175

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import java.util.*
 
 plugins {
     java
-    id("xyz.wagyourtail.unimined") version "1.3.7" apply false
+    id("xyz.wagyourtail.unimined") version "1.3.9" apply false
     id("xyz.wagyourtail.jvmdowngrader") version "1.1.2"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
     id("com.gradleup.shadow") version "8.3.0" apply false
