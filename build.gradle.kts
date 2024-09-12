6aaf5071d7f732b5f8cbaea0b262c7f64ab02902
CDAGaming <cstack2011@yahoo.com>
2024-09-12 10:48:09 -0500

    [Change] Updated Dependencies
    - JVMDowngrader updated to 1.1.3
    - Fabric Loader updated to 0.16.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import java.util.*
 plugins {
     java
     id("xyz.wagyourtail.unimined") version "1.3.9" apply false
-    id("xyz.wagyourtail.jvmdowngrader") version "1.1.2"
+    id("xyz.wagyourtail.jvmdowngrader") version "1.1.3"
     id("com.diffplug.gradle.spotless") version "6.25.0" apply false
     id("com.gradleup.shadow") version "8.3.0" apply false
     id("com.hypherionmc.modutils.modpublisher") version "2.1.6" apply false
