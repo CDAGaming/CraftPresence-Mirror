b6096a43ee40e459917f200aaedff1c3309af987
CDAGaming <cstack2011@yahoo.com>
2024-11-26 00:26:14 -0600

    [Port] Update to 1.19.2 (v2.5.2)

--- forge/src/main/java/com/gitlab/cdagaming/craftpresence/forge/CraftPresenceForge.java
+++ forge/src/main/java/com/gitlab/cdagaming/craftpresence/forge/CraftPresenceForge.java
@@ -34,7 +34,6 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.network.NetworkConstants;
 
 /**
  * The Primary Application Class and Utilities
@@ -55,7 +54,7 @@ public class CraftPresenceForge {
             try {
                 // Workaround: Client-side only fix for Forge Clients
                 // - Reference => https://gitlab.com/CDAGaming/CraftPresence/-/issues/99
-                ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+                ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
             } catch (Throwable ignored) {
                 // before forge-1.13.2-25.0.103
             }
