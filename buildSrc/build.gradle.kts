025656f00b6c25a47415cbcf7a28348c4ce63a9f
CDAGaming <cstack2011@yahoo.com>
2024-12-18 13:27:41 -0600

    [Fix] Fixed CI Error

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,6 @@ dependencies {
     implementation("org.ow2.asm:asm-tree:9.7.1")
     implementation("org.ow2.asm:asm-util:9.7.1")
     implementation("org.ow2.asm:asm-analysis:9.7.1")
+
+    implementation("org.apache.commons:commons-compress:1.27.1")
 }
