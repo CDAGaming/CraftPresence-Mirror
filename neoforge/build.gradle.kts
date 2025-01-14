a79c41ed8e21f168adc12f4bcb8f4dfdc77f5ae2
CDAGaming <cstack2011@yahoo.com>
2025-01-14 14:17:02 -0600

    [Port] Update to 1.20.2 (v2.5.3)

---
+++ neoforge/build.gradle.kts
@@ -0,0 +1,121 @@
+import xyz.wagyourtail.unimined.api.minecraft.task.RemapJarTask
+
+/**
+ * Retrieve a Project Property
+ */
+operator fun String.invoke(): String? {
+    return project.properties[this] as String?
+}
+
+val modName: String by extra
+val modId: String by extra
+
+val isLegacy: Boolean by extra
+val protocol: Int by extra
+val isJarMod: Boolean by extra
+val accessWidenerFile: File by extra
+val isMCPJar: Boolean by extra
+val isModern: Boolean by extra
+val versionFormat: String by extra
+val versionLabel: String by extra
+val mcVersionLabel: String by extra
+val mcMappingsType: String by extra
+val canUseATs: Boolean by extra
+val baseVersionLabel: String by extra
+
+val forgeVersion = "neoforge_version"()!!
+
+unimined.minecraft {
+    if (!isJarMod) {
+        neoForge {
+            if (canUseATs) {
+                accessTransformer(aw2at(accessWidenerFile))
+            }
+            loader(forgeVersion)
+            customSearge = (mcMappingsType != "mojmap" && mcMappingsType != "parchment")
+        }
+    } else {
+        jarMod {}
+    }
+}
+
+val common: Configuration by configurations.creating
+val shadowCommon: Configuration by configurations.creating
+
+configurations.compileClasspath.get().extendsFrom(common)
+configurations.runtimeClasspath.get().extendsFrom(common)
+
+dependencies {
+    if (isJarMod) {
+        "jarMod"("risugami:modloader:$forgeVersion")
+    }
+
+    common(project(path = ":common")) { isTransitive = false }
+    common(project(path = ":common", configuration = "shade"))
+    common(project(path = ":common", configuration = "runtime"))
+    shadowCommon(project(path = ":common", configuration = "shadeOnly"))
+    shadowCommon(project(path = ":common")) { isTransitive = false }
+}
+
+val resourceTargets = listOf(
+    "mcmod.info",
+    "META-INF/mods.toml",
+    "META-INF/neoforge.mods.toml",
+    "mod_$modName.info",
+    "pack.mcmeta"
+)
+val replaceProperties = mapOf(
+    "mod_id" to modId,
+    "mod_name" to modName,
+    "version" to baseVersionLabel,
+    "mcversion" to mcVersionLabel,
+    "forge_id" to name,
+    "fml_version_range" to "neo_fml_version_range"(),
+    "game_version_range" to "neoforge_game_version_range"(),
+    "loader_version_range" to "neoforge_loader_version_range"(),
+    "unilib_min_version" to "unilib_minimum_version"()!!
+)
+
+tasks.processResources {
+    inputs.properties(replaceProperties)
+
+    filesMatching(resourceTargets) {
+        expand(replaceProperties)
+    }
+}
+
+tasks.shadowJar {
+    mustRunAfter(project(":common").tasks.shadowJar)
+    dependsOn(project(":common").tasks.shadowJar)
+    from(zipTree(project(":common").tasks.shadowJar.get().archiveFile))
+    configurations = listOf(shadowCommon)
+    archiveClassifier.set("dev-shadow")
+}
+
+tasks.named<RemapJarTask>("remapJar") {
+    dependsOn(tasks.shadowJar.get())
+    asJar {
+        inputFile.set(tasks.shadowJar.get().archiveFile)
+        archiveClassifier.set(project.name)
+    }
+}
+
+tasks.jar {
+    if (canUseATs) {
+        manifest {
+            attributes(
+                mapOf(
+                    "FMLAT" to "accesstransformer.cfg"
+                )
+            )
+        }
+    }
+    archiveClassifier.set("dev")
+}
+
+tasks.sourcesJar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    val commonSources = project(":common").tasks.sourcesJar
+    dependsOn(commonSources)
+    from(commonSources.get().archiveFile.map { zipTree(it) })
+}
