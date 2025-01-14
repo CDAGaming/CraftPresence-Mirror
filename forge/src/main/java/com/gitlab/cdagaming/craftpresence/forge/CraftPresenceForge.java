ffb728d935236e7397fb696a712dfa64db632003
CDAGaming <cstack2011@yahoo.com>
2025-01-14 14:14:19 -0600

    [Port] Update to 1.19 (v2.5.3)

--- forge/src/main/java/com/gitlab/cdagaming/craftpresence/forge/CraftPresenceForge.java
+++ forge/src/main/java/com/gitlab/cdagaming/craftpresence/forge/CraftPresenceForge.java
@@ -28,7 +28,7 @@ import com.gitlab.cdagaming.craftpresence.CraftPresence;
 import com.gitlab.cdagaming.craftpresence.config.gui.MainGui;
 import com.gitlab.cdagaming.unilib.core.CoreUtils;
 import io.github.cdagaming.unicore.utils.OSUtils;
-import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -62,7 +62,7 @@ public class CraftPresenceForge {
 
             try {
                 // Register The Config GUI Factory, used in Forge for Mod Menu integration
-                ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((mc, parentScreen) -> new MainGui(parentScreen)));
+                ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, parentScreen) -> new MainGui(parentScreen)));
             } catch (Throwable ex) {
                 CoreUtils.LOG.error("Failed to register Config GUI Factory for @MOD_NAME@.", ex);
             }
