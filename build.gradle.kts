faf4e6518c69ae2b3a07a25efa2c47ab069a1bc0
CDAGaming <cstack2011@yahoo.com>
2025-04-15 11:21:11 -0500

    [Port] Update to 1.20.6 (v2.5.5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -269,7 +269,7 @@ subprojects {
                     devFallbackNamespace("searge")
                 }
             } else if (usingIntermediary) {
-                devFallbackNamespace("intermediary")
+                devFallbackNamespace("official")
             }
 
             if (shouldDowngrade) {
