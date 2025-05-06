354fccdbb74b0f9957b12f5c0b879b7a242246ce
CDAGaming <cstack2011@yahoo.com>
2025-05-06 14:57:32 -0500

    [Port] Update to 1.6.4 (v2.6.1)

--- common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/entity/EntityUtils.java
+++ common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/entity/EntityUtils.java
@@ -258,8 +258,9 @@ public class EntityUtils implements ExtendedModule {
 
     @Override
     public void getInternalData() {
-        if (!EntityList.method_8367().isEmpty()) {
-            for (Object entityLocationObj : EntityList.method_8367()) {
+        final Map mappings = (Map) StringUtils.getField(EntityList.class, null, "classToStringMapping", "field_75626_c", "field_3269", "c");
+        if (!mappings.values().isEmpty()) {
+            for (Object entityLocationObj : mappings.values()) {
                 final String entityLocation = (String) entityLocationObj;
                 if (entityLocation != null) {
                     final String entityName = StringUtils.getOrDefault(entityLocation, "generic");
