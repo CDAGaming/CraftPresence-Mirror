3a1a8836309afb250269cb0ca5291b3da5274c24
CDAGaming <cstack2011@yahoo.com>
2025-06-01 19:32:38 -0500

    [Port] Update to special/reindev-2.9 (v2.6.2)

--- common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/gui/GuiUtils.java
+++ common/src/main/java/com/gitlab/cdagaming/craftpresence/utils/gui/GuiUtils.java
@@ -32,8 +32,8 @@ import com.gitlab.cdagaming.unilib.utils.GameUtils;
 import io.github.cdagaming.unicore.utils.FileUtils;
 import io.github.cdagaming.unicore.utils.MappingUtils;
 import io.github.cdagaming.unicore.utils.StringUtils;
-import net.minecraft.src.client.gui.GuiContainer;
-import net.minecraft.src.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiContainer;
+import net.minecraft.client.gui.GuiScreen;
 import unilib.external.io.github.classgraph.ClassInfo;
 
 import java.util.List;
